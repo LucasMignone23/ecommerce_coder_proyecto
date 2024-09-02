62b98be0ed55303ccf8da47c585eb15a26bea683
LucasMignone23 <lucasmignone.lm@gmail.com>
2024-09-02 20:40:51 -0300

    Mas pruebas MUI

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,8 +1,10 @@
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 export const Home = () => {
   return (
     <div>
+      <Button variant="contained">Contained</Button>
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
     </div>
   );
