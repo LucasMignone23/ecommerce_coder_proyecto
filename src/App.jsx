03ff32c409786b7b0bf647a528dcbe45d70e2ac0
LucasMignone23 <lucasmignone.lm@gmail.com>
2024-10-04 15:25:25 -0300

    instalacion react-router-dom y creacion de item detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Navbar } from "./components/layouts/navbar/navbar";
+import ItemDetailContainer from "./components/pages/itemDetail/itemDetailContainer";
 import { ItemListContainer } from "./components/pages/itemListContainer/itemListContainer";
+
 
 function App() {
   let saludo = "¿Hola, Como estas?";
   return (
     <div>
       <Navbar />
-      <ItemListContainer greeting={saludo} />
+      {/*<ItemListContainer greeting={saludo} />*/}
+      <ItemDetailContainer />
     </div>
   );
 }
---
+++ src/components/pages/itemDetail/ItemDetailContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ItemDetail from './itemDetail';
+
+const itemDetailContainer = () => {
+  return <ItemDetail/>;
+}
+
+export default itemDetailContainer
